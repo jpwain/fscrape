dadef490e45bb485119cddc0eed604bc631eca0f
Abe Fettig <abe@fettig.net>
2012-12-02 20:17:50 -0500

    Update to read URLs from file, download images, and generate output as index.html

--- fscrape.py
+++ fscrape.py
@@ -1,22 +1,48 @@
+#!/usr/bin/env python
+
 # Scrape some info from Fancy
 
+import hashlib
 import urllib2
+import os.path
 from bs4 import BeautifulSoup
+from jinja2 import Template
 
-urlist = ['http://www.thefancy.com/things/235842739','http://www.thefancy.com/things/226011919083903237', 'http://www.thefancy.com/things/225361396508920913']
+# Load and compile template
+template = Template(file('template.html').read())
 
-for item in urlist:
+# Make sure images/ dir exists
+imagesDir = 'images'
+if not os.path.exists('images'):
+    os.mkdir('images')
 
-	page = urllib2.urlopen(item).read()
+# Process pages to populate items list
+items = []
+for url in file('urls.txt'):
+    url = url.strip()
+    print 'Processing page at %s' % url
+    page = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(page)
 
-	soup = BeautifulSoup(page)
+    items.append({
+        'title': soup.figcaption.string,
+        'imageUrl': soup.find(class_='fig-image').img['src'],
+        'price': soup.find(class_='price').string,
+    })
 
-	itemTitle = soup.figcaption.string
-	itemImageURL = soup.find(class_='fig-image').img['src']
-	itemPrice = soup.find(class_='price').string
+# For each item, download image
+for item in items:
+    filename = hashlib.md5(item['imageUrl']).hexdigest()
+    imagePath = 'images/%s' % filename
+    item['imageFilename'] = filename
+    if not os.path.exists(imagePath):
+        webImage = urllib2.urlopen(item['imageUrl'])
+        print 'Downloading image %s' % item['imageUrl']
+        imageFile = file(imagePath, 'w+b')
+        imageFile.write(webImage.read())
+        imageFile.close()
+    else:
+        print 'Using previously saved image for %s' % item['imageUrl']
 
-	print itemTitle
-	print itemImageURL
-	print itemPrice
-
-	# missing: use curl/wget to download the image for each item too, using itemImageURL
+output = template.render(items=items)
+file('index.html', 'w').write(output)
